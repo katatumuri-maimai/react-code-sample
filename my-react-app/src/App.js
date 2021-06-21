ebee9eaac748f98f7ea4ec1f1b3c6347cd01a08a
DESKTOP-TN66GDV\mymai <mm.katatumuri.48@gmail.com>
2021-06-21 09:54:36 +0900

    test

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter} from 'react-router-dom'
 import Main from './_components/Main';
 import GlobalNav from './_components/GlobalNav';
 
+console.log(window.location.href);
+console.log(window.history.state);
+
 function App() {
   return (
   <BrowserRouter>
